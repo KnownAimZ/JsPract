d761d3ccbd7bfb180a634277b790dfb860b2305c
Nikita <nbokii18@gmail.com>
2020-11-10 20:27:03 +0200

    Lesson_16_start

--- js/script3.js
+++ js/script3.js
@@ -9,7 +9,7 @@ const personalMoviesDB = {
     privat: false
 };
 
-for(let i = 0; i < 2; i++ ){
+/*for(let i = 0; i < 2; i++ ){
     const filmName = prompt("Один из последних просмотренных фильмов?","");
     const filmRank = +(prompt("На сколько его оцените?",""));
     if (filmName!="" && filmRank != "" && filmName.length<=50){
@@ -17,7 +17,7 @@ for(let i = 0; i < 2; i++ ){
     } else {
         i--;
     }  
-}
+}*/
 
 /**let i = 0;
 while (i < 2) {
@@ -28,7 +28,7 @@ personalMoviesDB.movies[filmName]=filmRank;
 i++; }
 }*/
 
-/*let i = 0;
+let i = 0;
 do {
     const filmName = prompt("Один из последних просмотренных фильмов?","");
     const filmRank = +(prompt("На сколько его оцените?",""));
@@ -36,7 +36,7 @@ do {
     personalMoviesDB.movies[filmName]=filmRank;
     i++;
     }  
-} while (i < 2);*/
+} while (i < 2);
 
 if(numberOfFilms < 10){
     alert("Просмотрено довольно мало фильмов");
