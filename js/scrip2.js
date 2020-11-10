2cb2a43b2f0a19f8110c3bb50ffdc0f5f862923d
Nikita <nbokii18@gmail.com>
2020-11-10 17:25:13 +0200

    DZ1 W2

--- js/scrip2.js
+++ js/scrip2.js
@@ -1,2 +1,12 @@
 let numberOfFilms = +(prompt("Сколько фильмов вы посмотрели?",""));
 
+let personalMoviesDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+console.log(personalMoviesDB);
+
