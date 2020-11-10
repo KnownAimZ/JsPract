edc5a50354bf79ddf929a0e48c912fcbbb18dfe8
Nikita <nbokii18@gmail.com>
2020-11-10 17:41:49 +0200

    DZ1 W3 final

--- js/scrip2.js
+++ js/scrip2.js
@@ -1,6 +1,6 @@
-let numberOfFilms = +(prompt("Сколько фильмов вы посмотрели?",""));
+const numberOfFilms = +(prompt("Сколько фильмов вы посмотрели?",""));
 
-let personalMoviesDB = {
+const personalMoviesDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
@@ -8,12 +8,12 @@ let personalMoviesDB = {
     privat: false
 };
 
-let filmName1 = prompt("Один из последних просмотренных фильмов?","");
-let filmRank1 = +(prompt("На сколько его оцените?",""));
-let filmName2 = prompt("Один из последних просмотренных фильмов?","");
-let filmRank2 = +(prompt("На сколько его оцените?",""));
+const filmName1 = prompt("Один из последних просмотренных фильмов?","");
+const filmRank1 = +(prompt("На сколько его оцените?",""));
+const filmName2 = prompt("Один из последних просмотренных фильмов?","");
+const filmRank2 = +(prompt("На сколько его оцените?",""));
 
-personalMoviesDB.movies[`${filmName1}`]=filmRank1;
-personalMoviesDB.movies[`${filmName2}`]=filmRank2;
+personalMoviesDB.movies[filmName1]=filmRank1;
+personalMoviesDB.movies[filmName2]=filmRank2;
 
 console.log(personalMoviesDB);
