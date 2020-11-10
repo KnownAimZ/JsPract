fd0ff35f4b26642c454d0ed00d31ffa2fffb8b48
Nikita <nbokii18@gmail.com>
2020-11-11 01:07:10 +0200

    Lesson_18

--- js/script4.js
+++ js/script4.js
@@ -1,6 +1,6 @@
 "use strict"
 
-let num = 20;
+/*let num = 20;
 
 function showFirstMessage(text) {
     console.log(text);
@@ -26,3 +26,38 @@ function ret() {
 
 const anotherNum = ret();
 console.log(anotherNum);
+
+const logger = function(){
+    console.log("Hello");
+};
+logger();
+
+const calq = (a, b) => a + b;*/
+
+/*const str = "test";
+console.log(str.length);
+
+const arr = [1, 2, 4];
+console.log(arr.length);*/
+
+
+const str1 = "teSt";
+console.log(str1.toUpperCase());
+console.log(str1.toLowerCase());
+console.log(str1);
+
+let fruit = "Some fruit";
+console.log(fruit.indexOf('fruit'));
+console.log(fruit.indexOf('q'));
+
+const logg="Hello World";
+console.log(logg.slice(6));
+console.log(logg.substring(6,11));
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
