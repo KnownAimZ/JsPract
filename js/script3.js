a0d43000bb607f8460814973600ae4fda8a27f20
Nikita <nbokii18@gmail.com>
2020-11-10 20:13:45 +0200

    DZ_Lesson_15

--- js/script3.js
+++ js/script3.js
@@ -1,8 +1,53 @@
 "use strict"
+const numberOfFilms = +(prompt("Сколько фильмов вы посмотрели?",""));
 
-let num = 50;
+const personalMoviesDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-while (num <= 55){
-    console.log(num);
-    num++;
+for(let i = 0; i < 2; i++ ){
+    const filmName = prompt("Один из последних просмотренных фильмов?","");
+    const filmRank = +(prompt("На сколько его оцените?",""));
+    if (filmName!="" && filmRank != "" && filmName.length<=50){
+    personalMoviesDB.movies[filmName]=filmRank;
+    } else {
+        i--;
+    }  
 }
+
+/**let i = 0;
+while (i < 2) {
+const filmName = prompt("Один из последних просмотренных фильмов?","");
+const filmRank = +(prompt("На сколько его оцените?",""));
+if (filmName!="" && filmRank != "" && filmName.length<=50){
+personalMoviesDB.movies[filmName]=filmRank;
+i++; }
+}*/
+
+/*let i = 0;
+do {
+    const filmName = prompt("Один из последних просмотренных фильмов?","");
+    const filmRank = +(prompt("На сколько его оцените?",""));
+    if (filmName!="" && filmRank != "" && filmName.length<=50){
+    personalMoviesDB.movies[filmName]=filmRank;
+    i++;
+    }  
+} while (i < 2);*/
+
+if(numberOfFilms < 10){
+    alert("Просмотрено довольно мало фильмов");
+} else if(numberOfFilms >= 10 && numberOfFilms <= 30){
+    alert("Вы классический зритель");
+} else if (numberOfFilms>30) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
+
+
+
+console.log(personalMoviesDB);
