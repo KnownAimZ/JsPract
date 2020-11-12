e7810413e6f32b7f56574c902bc515b7c049e78a
Nikita <nbokii18@gmail.com>
2020-11-13 01:40:30 +0200

    Lesson 24_DZ

--- js/script3.js
+++ js/script3.js
@@ -1,70 +1,75 @@
 "use strict"
 let numberOfFilms;
 
-start();
-
 const personalMoviesDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-};
-
-rememberMyFilms();
-
-detectPersonalLevel();
-
-showMyDB();
-
-writeYourGenres();
-
-function showMyDB() {
-    if (personalMoviesDB.privat == false) {
-        console.log(personalMoviesDB);
-    }
-}
-
-function writeYourGenres() {
-    let genres =[];
-    for (let j = 1; j <= 3; j++) {
-        let tempGenre = prompt(`Ваш любимый жанр под номером ${j}?`,"");
-        while(tempGenre == '' || tempGenre == null) {
-            tempGenre = prompt(`Ваш любимый жанр под номером ${j}?`,"");
-        } 
-        genres.push(tempGenre);       
-    }
-    console.log(genres);
-}
-
-function detectPersonalLevel() {
-    if(numberOfFilms < 10){
-        alert("Просмотрено довольно мало фильмов");
-    } else if(numberOfFilms >= 10 && numberOfFilms <= 30){
-        alert("Вы классический зритель");
-    } else if (numberOfFilms>30) {
-        alert("Вы киноман");
-    } else {
-        alert("Произошла ошибка");
-    }
-}
-
-function start() {
-    numberOfFilms = +(prompt("Сколько фильмов вы посмотрели?",""));
-
-    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    privat: false, 
+    showMyDB: function(){
+        if (this.privat == false) {
+            console.log(this);
+        }
+    },
+    writeYourGenres: function() {
+        
+        for (let j = 1; j <= 3; j++) {
+            let tempGenre = prompt(`Ваш любимый жанр под номером ${j}?`,"");
+            while(tempGenre == '' || tempGenre == null) {
+                tempGenre = prompt(`Ваш любимый жанр под номером ${j}?`,"");
+            } 
+            this.genres.push(tempGenre);       
+        }
+        this.genres.forEach(function(item, i) {
+            console.log(`Любимый жанр #${i + 1} - это ${item}`);
+        });
+    },
+    detectPersonalLevel: function() {
+        if(this.count < 10){
+            alert("Просмотрено довольно мало фильмов");
+        } else if(this.count >= 10 && this.count <= 30){
+            alert("Вы классический зритель");
+        } else if (this.count>30) {
+            alert("Вы киноман");
+        } else {
+            alert("Произошла ошибка");
+        }
+    },
+    start: function() {
         numberOfFilms = +(prompt("Сколько фильмов вы посмотрели?",""));
-    }
-}
 
-function rememberMyFilms() {
-    let i = 0;
-    do {
-        const filmName = prompt("Один из последних просмотренных фильмов?","");
-        const filmRank = +(prompt("На сколько его оцените?",""));
-        if (filmName!="" && filmRank != "" && filmName.length<=50){
-        personalMoviesDB.movies[filmName]=filmRank;
-        i++;
-        }  
-    } while (i < 2);
-}
+        while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+            numberOfFilms = +(prompt("Сколько фильмов вы посмотрели?",""));
+        }
+        this.count = numberOfFilms;
+    },
+    rememberMyFilms: function() {
+        let i = 0;
+        do {
+            const filmName = prompt("Один из последних просмотренных фильмов?","");
+            const filmRank = +(prompt("На сколько его оцените?",""));
+            if (filmName!="" && filmRank != "" && filmName.length<=50){
+                this.movies[filmName]=filmRank;
+            i++;
+            }  
+        } while (i < 2);
+    },
+    toggkeVisibleMyDB: function() {
+        if(this.privat == true) {
+            this.privat = false;
+        } else {
+            this.privat = true;
+        }
+    }
+};
+personalMoviesDB.start();
+
+personalMoviesDB.rememberMyFilms();
+
+personalMoviesDB.detectPersonalLevel();
+
+personalMoviesDB.showMyDB();
+
+personalMoviesDB.writeYourGenres();
+
--- js/script6.js
+++ js/script6.js
@@ -97,3 +97,24 @@ console.log(typeof(str));
 console.log(typeof(strObj));
 
 console.dir([1, 2, 3]);
+
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        console.log("Hello");
+    }
+};
+
+const john = Object.create(soldier);
+john.sayHello();
+
+/*const john = {
+    health: 100
+};
+
+//john.__proto__ = soldier; //устарело
+Object.setPrototypeOf(john, soldier);
+
+//console.log(john.armor);
+john.sayHello();*/
