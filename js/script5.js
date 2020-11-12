4508ed734c4e26cfb7c85fe28f080d3ea7f98413
Nikita <nbokii18@gmail.com>
2020-11-12 19:06:29 +0200

    Lesson_21

--- js/script5.js
+++ js/script5.js
@@ -25,26 +25,30 @@ function done() {
 
 learnJS('JavaScript', done);*/
 
-const options = {
+/*const options = {
     name: 'test',
     width: 1024,
     height: 1024,
     colors: {
         border: 'black',
         bg: 'red'
+    },
+    makeTest: function() {
+        console.log("Test");
     }
 };
 
-/*console.log(options.name);
+options.makeTest();
+
+console.log(options.name);
 delete options.name;
-console.log(options);*/
+console.log(options);
 
 let counter = 0;
 for (let key in options) {
     if(typeof(options[key]) === 'object') {
         for (let i in options[key]) {
-            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-            counter++;
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);   
         }
     }
     else {
@@ -54,4 +58,50 @@ for (let key in options) {
 }
 console.log(counter);
 
+console.log(Object.keys(options).length);
 
+const {border, bg} = options.colors;
+console.log(border); */
+
+/*const arr = [1, 2, 3, 6, 8];
+
+arr.pop();
+console.log(arr);
+
+arr.push(10);
+console.log(arr);
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+for(let value of arr) {
+    console.log(value);
+}*/
+
+/*const arr1 = [1, 2, 3, 6, 8, 10];
+
+arr1.forEach(function(item, i, arr1) {
+    console.log(`${i}: ${item} внутри массива ${arr1}`);
+});
+
+arr1[99] = 0;
+console.log(arr1.length);
+console.log(arr1);*/
+
+/*const str = prompt("","");
+const products = str.split(", ");
+products.sort();
+console.log(products.join('; '));*/
+
+const arr2 = [15, 25, 123, 6, 68, 10];
+
+arr2.sort();
+console.log(arr2);
+
+function compareNum(a, b) {
+    return a - b;
+}
+
+arr2.sort(compareNum);
+console.log(arr2);
