95fed4e01eedcd139277e2588f7808d1f794b219
Nikita <nbokii18@gmail.com>
2020-11-13 19:04:28 +0200

    Lesson 26

--- js/script7.js
+++ js/script7.js
@@ -1,22 +1,36 @@
 "use strict"
 
-function hello() {
-    console.log("Hello World");
-}
+// To String
+//1)
+//console.log(typeof(String(null)));
+//2)
+//console.log(typeof(5 + ''));
+//const num = 5;
+//console.log("https://vk.com/catalog/" + num);
+//const fontSize = 26 + 'px';
 
-hello();
+// To Number
+//1)
+//console.log(typeof(Number('4')));
+//2
+//console.log(typeof(+'4'));
+//3
+//console.log(typeof(parseInt('15px', 10)));
+//let answ = +prompt("Hello", "");
 
-function hi() {
-    console.log('Say hi');
-}
-
-hi();
-
-const arr = [1, 14, 4 ,30, 54],
-    sorted = arr.sort(compareNum);
-
-function compareNum(a, b) {
-    return a - b;
-}
-
-console.log(sorted);
+//To Boolean
+//false
+//0, '', null, undefined, NaN;
+//1)
+//let switcher = null;
+//if(switcher) {
+//    console.log('Working...');
+//}
+//switcher=1;
+//if(switcher) {
+//    console.log('Working...');
+//}
+//2)
+//console.log(typeof(Boolean('4')));
+//3)
+//console.log(typeof(!!'4'));
